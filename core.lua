03d59316faf847db045cba84dc5b19fc9faf2a87
zerpzero <102732901+zerpzero@users.noreply.github.com>
2022-03-31 12:59:14 +0200

    A LE TONK

--- core.lua
+++
@@ -1,7 +0,0 @@
-local media = LibStub("LibSharedMedia-3.0")
-
-do
-	local SOUND = media.MediaType and media.MediaType.SOUND or "sound"
-
-	media:Register(SOUND, "LETONK", "Interface\\AddOns\\!mdsounds\\Sounds\\letonk.ogg")
-end
